cda0f95a4dec1f7ed9efa4cd1747965ef4ed34cd
DavidBetteridge <davidabetteridge@gmail.com>
2017-03-01 10:11:26 +0000

    Simple

---
+++ Davids Example/Board.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Davids_Example
+{
+    internal class Board
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        private bool[,] cells;
+
+        public const bool ALIVE = true;
+        public const bool DEAD = false;
+
+        /// <summary>
+        /// Initialises a new board
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public Board(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.cells = new bool[this.Width, this.Height];
+        }
+
+        /// <summary>
+        /// Returns a new board with the same dimensions as this board but
+        /// containing the next generation of cells
+        /// </summary>
+        /// <returns></returns>
+        public Board NextGeneration()
+        {
+            var next = new Board(this.Width, this.Height);
+            for (int x = 0; x < this.Width; x++)
+            {
+                for (int y = 0; y < this.Height; y++)
+                {
+                    var liveCell = this.cells[x, y];
+                    var deadCell = !liveCell;
+                    var numberOfNeighbours = CountNeighbours(x, y);
+
+                    //Any live cell with fewer than two live neighbours dies, as if caused by underpopulation
+                    if (liveCell && numberOfNeighbours < 2)
+                        next.SetCell(x, y, DEAD);
+
+                    //Any live cell with more than three live neighbours dies, as if by overcrowding.
+                    if (liveCell && numberOfNeighbours > 3)
+                        next.SetCell(x, y, DEAD);
+
+                    //Any live cell with two or three live neighbours lives on to the next generation.
+                    if (liveCell && (numberOfNeighbours == 2 || numberOfNeighbours == 3))
+                        next.SetCell(x, y, ALIVE);
+
+                    //Any dead cell with exactly three live neighbours becomes a live cell.
+                    if (deadCell && numberOfNeighbours == 3)
+                        next.SetCell(x, y, ALIVE);
+                }
+            }
+
+            return next;
+        }
+
+        /// <summary>
+        /// Returns the current value of a cell
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public bool GetCell(int x, int y)
+        {
+            if (x < 0 || x > this.Width - 1) throw new ArgumentOutOfRangeException(nameof(x));
+            if (y < 0 || y > this.Height - 1) throw new ArgumentOutOfRangeException(nameof(y));
+
+            return this.cells[x, y];
+        }
+
+        /// <summary>
+        /// Sets the value of this single cell
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="alive"></param>
+        public void SetCell(int x, int y, bool alive = true)
+        {
+            if (x < 0 || x > this.Width - 1) throw new ArgumentOutOfRangeException(nameof(x));
+            if (y < 0 || y > this.Height - 1) throw new ArgumentOutOfRangeException(nameof(y));
+
+            this.cells[x, y] = alive;
+        }
+
+        /// <summary>
+        /// How many live neighbours does this cell work?
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public byte CountNeighbours(int x, int y)
+        {
+            if (x < 0 || x > this.Width - 1) throw new ArgumentOutOfRangeException(nameof(x));
+            if (y < 0 || y > this.Height - 1) throw new ArgumentOutOfRangeException(nameof(y));
+
+            var number = 0;
+
+            //Up
+            if (y > 0) number += this.cells[x, y - 1] ? 1 : 0;
+            if (y > 0 && x > 0) number += this.cells[x - 1, y - 1] ? 1 : 0;
+            if (y > 0 && x < this.Width - 1) number += this.cells[x + 1, y - 1] ? 1 : 0;
+
+            //Down
+            if (y < this.Height - 1) number += this.cells[x, y + 1] ? 1 : 0;
+            if (y < this.Height - 1 && x > 0) number += this.cells[x - 1, y + 1] ? 1 : 0;
+            if (y < this.Height - 1 && x < this.Width - 1) number += this.cells[x + 1, y + 1] ? 1 : 0;
+
+            //Left/Right
+            if (x > 0) number += this.cells[x - 1, y] ? 1 : 0;
+            if (x < this.Width - 1) number += this.cells[x + 1, y] ? 1 : 0;
+
+            return (byte)number;
+        }
+    }
+}
---
+++ Davids Example/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace Davids_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Number of cells we wish to display across the screen.
+            const int ScreenWidth = 60;
+
+            // Number of rows we wish to display down the screen
+            const int ScreenHeight = 20;
+
+            // Blinker (period 2)
+            var board = new Board(ScreenWidth, ScreenHeight);
+            board.SetCell(1, 8);
+            board.SetCell(2, 8);
+            board.SetCell(3, 8);
+
+            // Glider
+            board.SetCell(20, 10);
+            board.SetCell(21, 10);
+            board.SetCell(22, 10);
+            board.SetCell(22, 9);
+            board.SetCell(21, 8);
+
+            while (true)
+            {
+                DisplayBoard(board);
+                board = board.NextGeneration();
+                Thread.Sleep(100);
+            }
+            
+        }
+
+        private static void DisplayBoard(Board board)
+        {
+            for (int y = 0; y < board.Height; y++)
+            {
+                var line = "";
+                for (int x = 0; x < board.Width; x++)
+                {
+                    line += board.GetCell(x, y) ? "X" : ".";
+                }
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
