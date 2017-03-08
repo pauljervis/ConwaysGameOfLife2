9995e179e7c34e38af22c700cf4ad4c2d0f7e6c0
Simon Foster <funkysi1701@gmail.com>
2017-03-08 20:01:05 +0000

    Basic Rules of Life

---
+++ SiPaul/SiPaul/LifeManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiPaul
+{
+    class LifeManager
+    {
+        readonly bool[,] cell;
+
+        public LifeManager(int width, int height)
+        {
+            cell = new bool[width, height];
+        }
+
+        //public void SetCell(int x, int y, bool status)
+        //{
+        //    cell[x, y] = status;
+        //}
+
+        public int this[int x, int y]
+        {
+            get
+            {
+                return cell[x, y] ? 1 : 0;
+            }
+            set
+            {
+                cell[x, y] = value > 0;
+            }
+        }
+
+        public void Step()
+        {
+            for (int i = 0; i < cell.GetLength(0); i++)
+            {
+                for (int j = 0; j < cell.GetLength(1); j++)
+                {
+                    int count = (this[i - 1, j - 1] + this[i - 1, j] + this[i - 1, j + 1] + this[i, j - 1] + this[i, j] + this[i, j + 1] + this[i + 1, j - 1] + this[i + 1, j] + this[i + 1, j + 1]);
+                    cell[i, j] = checkAlive(cell[i, j], count);
+                }
+            }
+        }
+
+        public bool checkAlive(bool current, int siblingCount)
+        {
+            return (siblingCount == 3 || current && siblingCount == 2);
+        }
+    }
+}
---
+++ SiPaul/UnitTestLife/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestLife")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestLife")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b0f0062-d86b-43bb-a177-2c3501a1b9d7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
