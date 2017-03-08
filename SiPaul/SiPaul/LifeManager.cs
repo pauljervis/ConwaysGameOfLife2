c26d21bf578bd3f391fe7c790c8da87f69f1cfde
Paul Jervis <jervis@12os.co.uk>
2017-03-08 23:55:07 +0000

    begin parallelism

--- SiPaul/SiPaul/LifeManager.cs
+++ SiPaul/SiPaul/LifeManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,35 @@ namespace SiPaul
 {
     public class LifeManager
     {
+        private class CellValue
+        {
+            readonly int _x;
+            readonly int _y;
+            readonly bool _value;
+
+            public CellValue(int x, int y, bool value)
+            {
+                _x = x;
+                _y = y;
+                _value = value;
+            }
+
+            public int GetX()
+            {
+                return _x;
+            }
+
+            public int GetY()
+            {
+                return _y;
+            }
+
+            public bool GetValue()
+            {
+                return _value;
+            }
+        }
+
         private bool[,] cell;
 
         public LifeManager(int width, int height)
@@ -50,6 +80,25 @@ namespace SiPaul
                 }
             }
             cell = nextcell;
+            return cell;
+        }
+
+        public bool[,] ParallelStep()
+        {
+            bool[,] nextcell = new bool[cell.GetLength(0), cell.GetLength(1)];
+
+            for(int i = 0; i < cell.GetLength(0); i++)
+            {
+                Parallel.For(0, cell.GetLength(1),
+                    j =>
+                    {
+                        int count = (this[i - 1, j - 1] + this[i - 1, j] + this[i - 1, j + 1] + this[i, j - 1] + this[i, j + 1] + this[i + 1, j - 1] + this[i + 1, j] + this[i + 1, j + 1]);
+                        nextcell[i, j] = CheckAlive(cell[i, j], count);
+                    });
+            }
+
+            cell = nextcell;
+
             return cell;
         }
 
